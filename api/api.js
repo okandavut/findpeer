f5d5faf30a57ca78784a72b543e149e0a9cc1964
okandavut <davut.okan@gmail.com>
2023-01-04 17:22:10 +0300

    updated api

--- api/api.js
+++ api/api.js
@@ -2,20 +2,18 @@ import axios from "axios";
 import shuffle from "../utils/shuffleHelper";
 
 async function getCategories() {
-  const res = await axios.get(
-    "https://findsupeerbackend.herokuapp.com/categories"
-  );
+  const res = await axios.get("https://findsupeer-api.onrender.com/categories");
   return res.data;
 }
 async function getPeers() {
-  const res = await axios.get("https://findsupeerbackend.herokuapp.com/peers");
+  const res = await axios.get("https://findsupeer-api.onrender.com/peers");
 
   return shuffle(res.data);
 }
 
 async function getSuperPeerData(username) {
   const res = await axios
-    .post("https://findsupeerbackend.herokuapp.com/getSuperpeerInfo", {
+    .post("https://findsupeer-api.onrender.com/getSuperpeerInfo", {
       username: username,
     })
     .catch((err) => {
@@ -27,7 +25,7 @@ async function getSuperPeerData(username) {
 async function getImageAsBase64(image) {
   const res = await axios
     .post(
-      "https://findsupeerbackend.herokuapp.com/getImageAsBase64",
+      "https://findsupeer-api.onrender.com/getImageAsBase64",
       {
         ImgUrl: image,
       },
